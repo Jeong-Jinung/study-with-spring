459f344ec089305bc5473c00f6bd7c1f65de1dc6
JeongJinUng <jinung.dev@gmail.com>
2024-03-28 22:39:31 +0900

    first commit

---
+++ src/main/java/study/jinung/withspring/StudyWithSpringApplication.java
@@ -0,0 +1,13 @@
+package study.jinung.withspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/jinung/withspring/StudyWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jinung.withspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
